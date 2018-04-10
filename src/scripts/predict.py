bcdabb41d74b98f2281184e8897c5acbc84c7e8b
Ketul Shah <ketulshah@Ketuls-MacBook-Air.local>
2018-04-10 17:35:17 +0530

    weight path changed

--- src/scripts/predict.py
+++ src/scripts/predict.py
@@ -66,7 +66,7 @@ def get_random_word(n=1, array=unique_words):
     return array[:n]
 
 # load weights
-weights_path = '_weights.hdf5'
+weights_path = '_weightsn.hdf5'
     
 model.load_weights(weights_path)
 model.compile(loss='categorical_crossentropy', optimizer='adam')
